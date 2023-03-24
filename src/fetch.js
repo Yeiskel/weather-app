0c2ab993b38e2ad1dffbd6764ac08f66003e1f04
Yeiskel <yeiskelcisneros@hotmail.com>
2023-03-24 03:29:37 -0400

    fixing netlify functions

--- src/fetch.js
+++ src/fetch.js
@@ -1,12 +1,12 @@
 import { city, showWeather } from "./main";
 
 export function fetch_data() {
-	fetch(`/.netlify/functions/fetch-city?city=${city}`)
+	fetch(`/.netlify/functions/fetch-city/fetch-city?city=${city}`)
 		.then((response) => response.json())
 		.then((city_data) => {
 			const city_name = city_data[0].name;
 			fetch(
-				`/.netlify/functions/fetch-weather?lat=${city_data[0].lat}&lon=${city_data[0].lon}`
+				`/.netlify/functions/fetch-weather/fetch-weather?lat=${city_data[0].lat}&lon=${city_data[0].lon}`
 			)
 				.then((response) => response.json())
 				.then((weather_data) => showWeather(weather_data, city_name));
